8d82e731bb09b5ff476b86e3449ec9ede3832305
Jana-SI <janaina.adolfo.ds@gmail.com>
2023-08-23 16:03:17 -0300

    08 - Resgatando parâmetros de URL

---
+++ 08_aula/resources/views/layouts/main.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>@yield('title')</title>
+        {{-- fonts --}}
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
+
+        {{-- bootstrap --}}
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+        
+        {{-- css --}}
+        <link rel="stylesheet" href="/css/styles.css">
+        <script src="/js/scripts.js"></script>
+    </head>
+    <body>
+        <header>
+            <nav class="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
+                <div class="collapse navbar-collapse" id="navbar">
+                    <a href="/" class="nnavbar-brand">
+                        <img src="/img/hdcevents_logo.svg" alt="HDC Eventos">
+                    </a>
+
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="/" class="nav-link">Eventos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/" class="nav-link">Criar Eventos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/" class="nav-link">Entrar</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/" class="nav-link">Cadastrar</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+
+        @yield('content')
+
+        <footer>
+            <p>HDC Eventos &copy; 2023</p>
+        </footer>
+    </body>
+</html>
---
+++ 08_aula/resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('title', 'HDC Eventos')
+
+@section('content')
+    
+    <h1>Aula 08 - Resgatando parâmetros de URL</h1>
+
+@endsection
